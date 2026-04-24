9495dd2d4cf61d54a27dd943dc16bff832b1e4f0
LenomiOff <lelelenomi@gmail.com>
2026-04-24 19:30:31 +0200

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const app = express();
-const PORT = 3000;
-
-app.get('/video', async (req, res) => {
-    const url = req.query.url;
-    // Ici tu peux faire fetch côté serveur pour "proxy" YouTube
-    res.redirect(url);
-});
-
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+cacaeffsegsre
